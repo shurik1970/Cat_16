d43d72119d9c59805be549da91fa399e53addaf3
Alexandr Matyushin <122092093+shurik1970@users.noreply.github.com>
2023-01-31 20:48:19 +0300

    Add files via upload

---
+++ testcat.py
@@ -0,0 +1,11 @@
+from cat import Cat
+
+pit = Cat("Барон", "мальчик", 2)
+pit1 = Cat("Сэм", "мальчик", 2)
+
+print("имя", pit.name)
+print(pit.gender)
+print(pit.age, "года")
+print("имя", pit1.name)
+print(pit1.gender)
+print(pit1.age, "года")
